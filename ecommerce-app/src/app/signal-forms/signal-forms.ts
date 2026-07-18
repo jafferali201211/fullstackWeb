7b8b520a186534378b0de274369da93165916830
Jaffer Ali <jafferali201211@gmail.com>
2026-07-19 00:19:06 +0530

    SignalForm created

---
+++ ecommerce-app/src/app/signal-forms/signal-forms.ts
@@ -0,0 +1,12 @@
+import { Component,signal } from '@angular/core';
+// import {form} from '@angular/forms/signal';
+
+@Component({
+  selector: 'app-signal-forms',
+  imports: [],
+  templateUrl: './signal-forms.html',
+  styleUrl: './signal-forms.css',
+})
+export class SignalForms {
+
+}
